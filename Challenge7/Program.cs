ca5a51744ce9b01bab6d2e981dbcdcf900b996ad
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2022-02-11 13:00:15 +0100

    added files

---
+++ Challenge7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TestPermutation("abd", "eaidbaooo"));
+        }
+        
+        static bool TestPermutation(string s1, string s2)
+        {
+            var permutationList = new List<string>();
+
+           GetPermutations(s1, permutationList);
+
+           return permutationList.Any(s2.Contains);
+        }
+        
+
+
+        static void GetPermutations(string text, List<string> permutations, string workingSet ="")
+        {
+          
+            if (text.Length == 0) permutations.Add(workingSet);
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char value = text.ElementAt(i);
+
+                workingSet += value;
+                text = text.Remove(i, 1);
+
+                GetPermutations(text, permutations, workingSet);
+
+                text = text.Insert(i, value.ToString());
+                workingSet = workingSet.Remove(workingSet.Length - 1, 1);
+            }
+        }
+    }
+}
---
+++ Permutation/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetValidParenthese(5);
+        }
+
+        static List<string> GetValidParenthese(int n)
+        {
+            string fullParentheses = "";
+
+            for (int i = 0; i < n; i++) fullParentheses += "(";
+            for (int i = 0; i < n; i++) fullParentheses += ")";
+            
+            var parentheseList = new List<string>();
+            GetPermutations(fullParentheses, parentheseList);
+
+            var validParentheseList = parentheseList.Where(IsValidParenthese).ToList();
+            
+            return validParentheseList;
+        }
+
+        static void GetPermutations(string text, List<string> permutations, string workingSet = "")
+        {
+            if (text.Length == 0 && !permutations.Contains(workingSet)) 
+                permutations.Add(workingSet);
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char value = text.ElementAt(i);
+
+                workingSet += value;
+                text = text.Remove(i, 1);
+
+                GetPermutations(text, permutations, workingSet);
+
+                text = text.Insert(i, value.ToString());
+                workingSet = workingSet.Remove(workingSet.Length - 1, 1);
+            }
+        }
+
+        static bool IsValidParenthese(string parenthese)
+        {
+            var stack = new Stack<char>();
+
+            foreach (char c in parenthese)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(c);
+                        break;
+                    case ')' when stack.Count == 0:
+                        return false;
+                    case ')':
+                        stack.Pop();
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
